a388eb2a56fb67e465f7beb43a9e6c510814ef71
jonschlinkert <jon.schlinkert@sellside.com>
2016-09-27 07:37:17 -0400

    more `options` unit tests

--- test/options.js
+++ test/options.js
@@ -10,7 +10,7 @@ function match(arr, pattern, expected, options) {
 }
 
 describe('options', function() {
-  describe('.ignore', function() {
+  describe('options.ignore', function() {
     it('should filter out ignored patterns', function() {
       var globs = ['a', 'a/a', 'a/a/a', 'a/a/a/a', 'a/a/a/a/a', 'a/a/b', 'a/b', 'a/b/c', 'a/c', 'a/x', 'b', 'b/b/b', 'b/b/c', 'c/c/c', 'e/f/g', 'h/i/a', 'x/x/x', 'x/y', 'z/z', 'z/z/z'];
       var globstars = ['a/a', 'a/b', 'a/c', 'a/x', 'a/x/y', 'a/x/y/z'];
@@ -30,6 +30,41 @@ describe('options', function() {
       match(negations, '!b/(a)', ['b/b', 'b/c'], opts);
       match(negations, '!(b/(a))', ['b/b', 'b/c'], opts);
       match(negations, '!(b/a)', ['b/b', 'b/c'], opts);
+    });
+  });
+
+  describe('options.matchBase', function() {
+    it('should support the `matchBase` option:', function() {
+      match(['a/b/c/d.md'], '*.md', []);
+      match(['a/b/c/d.md'], '*.md', ['a/b/c/d.md'], {matchBase: true});
+    });
+  });
+
+  describe('options.nocase', function() {
+    it('should support `options.nocase` option:', function() {
+      match(['a/b/d/e.md'], 'a/b/c/*.md', []);
+      match(['a/b/c/e.md'], 'A/b/C/*.md', []);
+      match(['a/b/c/e.md'], 'A/b/C/*.md', ['a/b/c/e.md'], {nocase: true});
+      match(['a/b/c/e.md'], 'A/b/C/*.MD', ['a/b/c/e.md'], {nocase: true});
+    });
+  });
+
+  describe('options.dot', function() {
+    it('should match dotfiles when `options.dot` is true:', function() {
+      match(['.dotfile'], '*.*', ['.dotfile'], {dot: true});
+      match(['.dotfile'], '*.md', [], {dot: true});
+      match(['.dotfile'], '.dotfile', ['.dotfile'], {dot: true});
+      match(['.dotfile.md'], '.*.md', ['.dotfile.md'], {dot: true});
+      match(['.verb.txt'], '*.md', [], {dot: true});
+      match(['.verb.txt'], '*.md', [], {dot: true});
+      match(['a/b/c/.dotfile'], '*.md', [], {dot: true});
+      match(['a/b/c/.dotfile.md'], '**/*.md', ['a/b/c/.dotfile.md'], {dot: true});
+      match(['a/b/c/.dotfile.md'], '**/.*', ['a/b/c/.dotfile.md']);
+      match(['a/b/c/.dotfile.md'], '**/.*.md', ['a/b/c/.dotfile.md']);
+      match(['a/b/c/.dotfile.md'], '*.md', []);
+      match(['a/b/c/.dotfile.md'], '*.md', [], {dot: true});
+      match(['a/b/c/.verb.md'], '**/*.md', ['a/b/c/.verb.md'], {dot: true});
+      match(['d.md'], '*.md', ['d.md'], {dot: true});
     });
   });
 });
