44aaae8edb5467ea01aeb04ff9b6359c95fe4764
jonschlinkert <jon.schlinkert@sellside.com>
2016-09-27 07:36:56 -0400

    relax `**/`

--- test/globstars.js
+++ test/globstars.js
@@ -1,7 +1,14 @@
 'use strict';
 
 var assert = require('assert');
+var argv = require('yargs-parser')(process.argv.slice(2));
 var mm = require('..');
+var matcher = argv.mm ? require('multimatch') : mm;
+
+function match(arr, pattern, expected, options) {
+  var actual = matcher(arr, pattern, options);
+  assert.deepEqual(actual.sort(), expected.sort());
+}
 
 describe('globstars', function() {
   it('should match double star patterns', function() {
@@ -16,5 +23,10 @@ describe('globstars', function() {
     assert(mm.isMatch('a/b/c/j/e/z/foo.md', 'a/**/j/**/z/*.md'));
     assert(mm.isMatch('a/b/z/.dotfile.md', '**/.*.md'));
     assert(mm.isMatch('a/b/z/.dotfile.md', 'a/**/z/.*.md'));
+  });
+
+  it('should match extensions:', function() {
+    match(['.md', 'a.md', 'a/b/c.md', '.txt'], '**/*.md', ['a.md', 'a/b/c.md']);
+    match(['.md', 'a/b/.md'], '**/.md', ['.md', 'a/b/.md']);
   });
 });
