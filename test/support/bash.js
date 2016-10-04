08743e58ca8223667d83381a97fbe63a83ecbbc8
jonschlinkert <jon.schlinkert@sellside.com>
2016-10-04 00:07:18 -0400

    improve test fixture creation
    it shouldn't fail on bad file patterns, we'll just skip them

--- test/support/bash.js
+++ test/support/bash.js
@@ -1,30 +1,17 @@
 'use strict';
 
+var path = require('path');
 var extend = require('extend-shallow');
 var bash = require('bash-glob');
 var del = require('delete');
 var write = require('./write');
 
-module.exports = function(fixtures, pattern, options, cb) {
-  if (typeof options === 'function') {
-    cb = options;
-    options = {};
-  }
-
+module.exports = function(fixtures, pattern, options) {
   fixtures = Array.isArray(fixtures) ? fixtures : [fixtures];
   var opts = extend({}, options);
   var content = opts.content || 'this is a fixture';
-
-  write(fixtures, content, opts, function(err, files) {
-    if (err) return cb(err);
-
-    bash(pattern, opts, function(err, matches) {
-      if (err) return cb(err);
-
-      del(files, {force: true}, function(err) {
-        if (err) return cb(err);
-        cb(null, matches);
-      });
-    });
-  });
+  opts.cwd = opts.cwd || path.join(__dirname, '../fixtures');
+  del.sync(opts.cwd);
+  write(fixtures, content, opts);
+  return bash.sync(pattern, opts);
 };
--- test/support/write.js
+++ test/support/write.js
@@ -1,33 +1,66 @@
 'use strict';
 
+var fs = require('fs');
 var path = require('path');
 var write = require('write');
+var mkdirp = require('mkdirp');
 var each = require('async-each');
+var isAbsolute = require('is-absolute');
+var del = require('delete');
 
-module.exports = function(fixtures, content, options, cb) {
-  if (typeof options === 'function') {
-    cb = options;
-    options = {};
-
-  }
-
+module.exports = function(fixtures, content, options) {
   fixtures = Array.isArray(fixtures) ? fixtures : [fixtures];
   var cwd = options.cwd || path.resolve(__dirname, '../fixtures');
   var files = [];
 
-  each(fixtures, function(fixture, next) {
-    var fp = path.resolve(cwd, fixture);
-    files.push(fp);
+  var len = fixtures.length;
+  var idx = -1;
 
-    write(fp, 'fixture', function(err) {
-      if (err && err.code !== 'EEXIST') {
-        next(err);
-        return;
-      }
-      next();
-    });
-  }, function(err) {
-    if (err) return cb(err);
-    cb(null, files);
-  });
+  while (++idx < len) {
+    var fp = path.resolve(cwd, fixtures[idx]);
+    var dir = path.dirname(fp);
+
+    if (!/test\/fixtures/.test(dir)) {
+      continue;
+    }
+
+    var dirStat = tryStat(dir);
+    if (dirStat === null) {
+      mkdirp.sync(dir);
+    } else if (dirStat.isFile()) {
+      del.sync(dir);
+    }
+
+    var stat = tryStat(fp);
+    if (stat && stat.isDirectory()) {
+      continue;
+    }
+
+    if (stat && stat.isFile()) {
+      continue;
+    }
+
+    files.push(fp);
+    try {
+      write.sync(fp, 'fixture');
+    } catch (err) {
+      // console.log(err);
+      // console.log(dirStat);
+      // console.log(stat);
+    }
+  }
+
+  return files;
 };
+
+function tryStat(fp) {
+  try {
+    return fs.lstatSync(fp);
+  } catch (err) {
+    if (/test\/fixtures\/./.test(fp)) {
+      del.sync(fp);
+      mkdirp.sync(fp);
+    }
+  }
+  return null;
+}
