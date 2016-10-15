c5cc4c43d4b254adf0637c196d6e9f0749192a90
jonschlinkert <jon.schlinkert@sellside.com>
2016-10-15 04:28:23 -0400

    simplify bash util

--- test/support/bash.js
+++ test/support/bash.js
@@ -1,17 +1,53 @@
 'use strict';
 
-var path = require('path');
-var extend = require('extend-shallow');
-var bash = require('bash-glob');
-var del = require('delete');
-var write = require('./write');
+var spawn = require('cross-spawn');
+var bashPath = process.env.BASH || '/usr/local/bin/bash';
 
-module.exports = function(fixtures, pattern, options) {
-  fixtures = Array.isArray(fixtures) ? fixtures : [fixtures];
-  var opts = extend({}, options);
-  var content = opts.content || 'this is a fixture';
-  opts.cwd = opts.cwd || path.join(__dirname, '../fixtures');
-  del.sync(opts.cwd);
-  write(fixtures, content, opts);
-  return bash.sync(pattern, opts);
+function bash(str, pattern, options) {
+  var cmd = pattern;
+
+  if (!/echo/.test(cmd)) {
+    cmd = `shopt -s extglob && shopt -s globstar && if [[ ${str} == ${pattern} ]]; then echo true; fi`;
+  }
+
+  var res = spawn.sync(bashPath, ['-c', cmd], options);
+  var err = toString(res.stderr);
+  if (err) {
+    console.error(cmd);
+    throw new Error(err);
+  }
+
+  return toString(res.stdout) || false;
+}
+
+
+bash.isMatch = function(fixture, pattern, options) {
+  return bash(fixture, pattern, options);
 };
+
+bash.match = function(fixtures, pattern) {
+  var matches = [];
+  var len = fixtures.length;
+  var idx = -1;
+  while (++idx < len) {
+    var fixture = fixtures[idx];
+    if (bash.isMatch(fixture, pattern)) {
+      matches.push(fixture);
+    }
+  }
+  return matches;
+};
+
+/**
+ * Stringify `buf`
+ */
+
+function toString(buf) {
+  return buf ? buf.toString().trim() : null;
+}
+
+/**
+ * Expose `bash`
+ */
+
+module.exports = bash;
