f769f8455c9621f17aca7f8bd103fe578e5e8030
jonschlinkert <jon.schlinkert@sellside.com>
2016-10-18 14:44:10 -0400

    adds examples

--- examples/makeRe.js
+++ examples/makeRe.js
@@ -1,3 +1,13 @@
+var mm = require('minimatch');
 var nm = require('..');
-console.log(nm.makeRe('*.js'));
-//=> /^(?:(\.[\\\/])?(?!\.)(?=.)[^\/]*?\.js)$/
+
+var re = nm.makeRe('*[a-b].[a-b]*');
+console.log(re);
+console.log(re.test('c.a'));
+
+var re = mm.makeRe('*[a-b].[a-b]*');
+console.log(re);
+console.log(re.test('c.a'));
+
+
+// console.log(/[^/]*?[a-b].[a-b][^/]*?/.test('c.a'))
---
+++ examples/paths.js
@@ -0,0 +1,6 @@
+var nm = require('..');
+
+var re = nm.makeRe('D://*');
+console.log(re);
+console.log(re.test('D://foo'));
+
