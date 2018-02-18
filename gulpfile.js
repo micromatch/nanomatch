7ae27588c409e62a7f54e8d60ca7906c6aeeaff2
jonschlinkert <jon.schlinkert@sellside.com>
2018-02-18 08:59:09 -0500

    lint, upgrade deps

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,6 @@
 
 var gulp = require('gulp');
 var mocha = require('gulp-mocha');
-var unused = require('gulp-unused');
 var istanbul = require('gulp-istanbul');
 
 gulp.task('coverage', function() {
@@ -15,11 +14,6 @@ gulp.task('test', ['coverage'], function() {
   return gulp.src('test/*.js')
     .pipe(mocha({reporter: 'spec'}))
     .pipe(istanbul.writeReports());
-});
-
-gulp.task('unused', function() {
-  return gulp.src(['index.js', 'lib/*.js'])
-    .pipe(unused({keys: Object.keys(require('./lib/utils.js'))}));
 });
 
 gulp.task('default', ['test']);
